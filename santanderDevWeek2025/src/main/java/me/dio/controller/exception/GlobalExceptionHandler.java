5bb31d2a17388856e7401d7d14e5e7980d31f526
capelli1983 <rdgcapelli@gmail.com>
2025-08-16 10:38:31 -0300

    Correções GlobalExceptionHandler

--- santanderDevWeek2025/src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
+++ santanderDevWeek2025/src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
@@ -9,28 +9,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
-
-
 @RestControllerAdvice
-public class globalExceptionHandler {
+public class GlobalExceptionHandler {
 
-    private final Logger logger =  LoggerFactory.getLogger(globalExceptionHandler.class);
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    @ExceptionHandler (IllegalArgumentException.class)
+    @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException) {
         return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
-    @ExceptionHandler (NoSuchElementException.class)
+    @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException) {
         return new ResponseEntity<>("Resource ID not found", HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler (Throwable.class)
-    public ResponseEntity<String> handleUnexpectedException(Throwable UnexpectedException) {
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException) {
         var message = "Unexpected server error, see the logs";
-        logger.error("", UnexpectedException);
-        return new ResponseEntity<>("Unexpected server error, see the logs", HttpStatus.INTERNAL_SERVER_ERROR);
+        logger.error(message, unexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
 }
+
